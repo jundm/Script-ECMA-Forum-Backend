5b879deabd663f2d6f4078d9a74a87cdfe980667
junboss <80582578+jundm@users.noreply.github.com>
2022-02-26 00:06:58 +0900

    Delete frontend directory

--- frontend/pages/404.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface NotFoundProps {}
-
-function NotFound({}: NotFoundProps) {
-  return <div>NotFound</div>;
-}
-
-export default NotFound;
--- frontend/pages/accounts/userinfo/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface UserInfoProps {}
-
-function UserInfo({}: UserInfoProps) {
-  return <div>UserInfo</div>;
-}
-
-export default UserInfo;
